4aefeb7f53993b9532499a565c5365997846c7c2
jean-philippe serafin <serafinjp@gmail.com>
2014-01-19 08:23:15 +0000

    add a POST / view that creates new handlers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     app = express(),
-    dotenv = require('dotenv');
+    dotenv = require('dotenv'),
+    db = require('./models');
 
 
 dotenv.load();
@@ -13,6 +14,16 @@ app.use(function (request, response, next) {
     "use strict";
     response.contentType('application/json');
     next();
+});
+
+
+app.post('/', function (request, response) {
+    "use strict";
+    var handler = db.Handler.build();
+
+    handler.save().success(function () {
+        response.json(201, handler);
+    });
 });
 
 
