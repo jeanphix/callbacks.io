554a453f770d407cd10fa4ceafb39068bc211a73
jean-philippe serafin <serafinjp@gmail.com>
2014-01-19 08:40:14 +0000

    add * /:id/listener view that store all incoming request details

--- app.js
+++ app.js
@@ -11,6 +11,28 @@ dotenv.load();
 app.set('port', process.env.PORT || 3000);
 
 
+app.use(express.logger('dev'));
+app.use(express.cookieParser());
+
+
+app.use(function (request, response, next) {
+    "use strict";
+    request.data = '';
+    request.text = '';
+    request.on('data', function (chunk) {
+        request.text += chunk;
+    });
+    request.on('end', function () {
+        try {
+            request.data = JSON.parse(request.text);
+        } catch (e) {
+
+        }
+        next();
+    });
+});
+
+
 app.use(function (request, response, next) {
     "use strict";
     response.contentType('application/json');
@@ -63,6 +85,19 @@ app.get('/:id', function (request, response) {
     var id = request.params.id[0];
     getHandlerOr404(response, id, function (handler) {
         response.json(200, handler);
+    });
+});
+
+
+app.all('/:id/listener', function (request, response) {
+    "use strict";
+    var id = request.params.id[0];
+    getHandlerOr404(response, id, function (handler) {
+        db.Callback.buildFromRequest(request, handler, function (callback) {
+            callback.save().success(function (callback) {
+                response.json(200, { message: 'success' });
+            });
+        });
     });
 });
 
