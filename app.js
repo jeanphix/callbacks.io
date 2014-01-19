78fb961299505a94086e5226453b231f9e57e716
jean-philippe serafin <serafinjp@gmail.com>
2014-01-19 08:35:12 +0000

    add a GET /:id view that describe en existing handler

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app = express(),
     dotenv = require('dotenv'),
-    db = require('./models');
+    db = require('./models'),
+    uuidRegexp = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
 
 
 dotenv.load();
@@ -23,6 +24,45 @@ app.post('/', function (request, response) {
 
     handler.save().success(function () {
         response.json(201, handler);
+    });
+});
+
+
+app.param(function (name, fn) {
+    "use strict";
+    if (fn instanceof RegExp) {
+        return function (request, response, next, value) {
+            var captures = fn.exec(String(value));
+            if (captures) {
+                request.params[name] = captures;
+                next();
+            } else {
+                next('route');
+            }
+        };
+    }
+});
+
+
+app.param('id', uuidRegexp);
+
+
+var getHandlerOr404 = function (response, id, callback) {
+    "use strict";
+    db.Handler.find(id).success(function (handler) {
+        if (handler === null) {
+            response.json(404, {error: 'Handler not found'});
+        }
+        callback(handler);
+    });
+};
+
+
+app.get('/:id', function (request, response) {
+    "use strict";
+    var id = request.params.id[0];
+    getHandlerOr404(response, id, function (handler) {
+        response.json(200, handler);
     });
 });
 
