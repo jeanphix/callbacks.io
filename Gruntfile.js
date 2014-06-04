df98a9fdb34d0ef9b72852be5c6954bfca03ec7a
jean-philippe serafin <serafinjp@gmail.com>
2014-06-04 18:22:21 +0000

    add a grunt server command

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,26 @@
 module.exports = function (grunt) {
     "use strict";
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            scripts: {
+                files: '**/*.js',
+                tasks: ['express:dev']
+            },
+            options: {
+                spawn: false
+            }
+        },
+        express: {
+            dev: {
+                options: {
+                    script: 'server.js'
+                }
+            }
+        }
     });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-express-server');
+
+    grunt.registerTask('server', [ 'express:dev', 'watch' ]);
 };
